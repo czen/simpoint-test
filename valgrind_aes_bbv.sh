d605c9c393f84dd0c9cd02d442323ffa282aeee0
Anton Bagliy <taccessviolation@gmail.com>
2018-10-04 12:10:33 +0000

    ADD: test script to run simpoint in valgrind bbv

--- valgrind_aes_bbv.sh
+++ valgrind_aes_bbv.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-valgrind --tool=exp-bbv --interval-size=1000 --bb-out-file=aes.bbv --pc-out-file=aes.pc ./aes
+cd CHstone/aes
+g++ aes.c -o aes
+cd ../../
+valgrind --tool=exp-bbv --interval-size=1000 --bb-out-file=simpoint/input/aes.bb --pc-out-file=simpoint/input/aes.pc ./CHstone/aes/aes
+
+cd simpoint
+make
+bin/simpoint -loadFVFile input/aes.bb -maxK 30 -saveSimpoints output/aes.simpoints -saveSimpointWeights output/aes.weights
